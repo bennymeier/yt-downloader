6928f4cb8d41049e8ea0ec23e359e67721f4fbda
Benny <meier.benjamin@web.de>
2019-04-06 20:29:51 +0200

    Use materialize css, use try/catch, check for valid yt url

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ const port = process.env.PORT || 4000;
 app.use(express.static(__dirname + '/public'));
 app.use(cors());
 app.use(express.json());
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+app.listen(port, () => console.log(`Server is running on port ${port}`));
 // serve the homepage
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -20,34 +18,43 @@ app.post('/metainfo', async (req, res) => {
     if (!videoURL) throw new Error("No video url!");
     try {
         let result = await promiseInfo(videoURL);
-        res.send(result);
+        res.send({ fail: false, ...result });
     } catch (err) {
+        res.send(err.message).status(500);
         throw err;
     }
 });
-
+// download the youtube video
 app.get("/download", async (req, res) => {
     let videoURL = req.query.URL;
+    let videoFormat = req.query.FORMAT || "mp4";
     let info;
     let title;
     try {
-        info = await promiseInfo(videoURL);
-        title = info.title.replace(/[^a-zA-Z0-9-äöü ]/g, " ");
+        info = await promiseInfo(videoURL)
+            .catch((err) => {
+                throw err;
+            });
     } catch (err) {
+        res.send(err.message).status(500);
         throw err;
     }
-    console.log("Titel: ", title);
-    res.header('Content-disposition', 'attachment; filename=' + title + '.mp4');
+    title = info.title.replace(/[^a-zA-Z0-9-äöü ]/g, " ");
+    res.header('Content-disposition', 'attachment; filename=' + title + "." + videoFormat);
     ytdl(videoURL, {
-        format: "mp4",
+        format: videoFormat,
     }).pipe(res);
 });
 
 const promiseInfo = (videoURL) => {
     return new Promise((resolve, reject) => {
         ytdl.getInfo(videoURL, (err, info) => {
-            if (err) reject(err);
+            if (err) {
+                reject("NO VALID URL!");
+            }
             resolve(info);
         });
+    }).catch((err) => {
+        console.log("Error 1");
     });
 }
--- public/script.js
+++ public/script.js
@@ -1,14 +1,21 @@
-const convertBtn = document.querySelector(".convert-button");
-const URLinput = document.querySelector(".URL-input");
+const convertBtn = document.getElementById("download-btn");
 convertBtn.addEventListener("click", () => {
-    sendURL(URLinput.value);
+    const videoURL = document.getElementById("download-input").value;
+    const formatInfo = document.querySelector("input[name='videoFormat']:checked").value;
+    fetchMetaInfo(videoURL);
+    const isValid = isValidYtUrl(videoURL);
+    if (!isValid) {
+        throw new Error("Invalid url!");
+    }
+    sendURL(videoURL, formatInfo);
 });
-const sendURL = (URL) => {
-    window.location.href = `http://localhost:4000/download?URL=${URL}`;
+const sendURL = (url, format) => {
+    window.location.href = `http://localhost:4000/download?URL=${url}&FORMAT=${format}`;
 };
-const metaBtn = document.querySelector(".meta-button");
-const metaValue = document.getElementById("meta-input");
-metaBtn.addEventListener("click", () => fetchMetaInfo(metaValue.value));
+const isValidYtUrl = (url) => {
+    const ytRegex = /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(\?\S*)?$/;
+    return url.match(ytRegex);
+};
 const fetchMetaInfo = (videoURL) => {
     fetch("/metainfo", {
         method: "POST", headers: {
@@ -18,17 +25,22 @@ const fetchMetaInfo = (videoURL) => {
         body: JSON.stringify({ videoURL })
     })
         .then((response) => {
-            if (response.ok) return response.json();
+            if (response.ok) {
+                return response.json();
+            }
             throw new Error("Request failed.");
         })
         .then((metainfo) => {
             const metaTitle = document.getElementById("meta-title");
+            if (metainfo.fail) {
+                metaTitle.innerText = "Video URL is not valid!";
+                return;
+            }
             metaTitle.innerHTML = metainfo.title;
             const metaThumbnail = document.getElementById("meta-thumbnail");
             metaThumbnail.src = metainfo.thumbnail_url;
-            console.log("data: ", metainfo);
         })
-        .catch((error) => {
-            console.log(error);
+        .catch((err) => {
+            throw err;
         });
 };
