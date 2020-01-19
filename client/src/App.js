05b42e86f563407c1c5dbf94c9b54915d7f06cf9
Benny <meier.benjamin@web.de>
2020-01-19 02:08:00 +0100

    Implement new card component, add related videos

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { TextField, Container, Button, Radio, RadioGroup, FormControlLabel, Typography } from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
 import API from "./utils/API";
 import SaveIcon from "@material-ui/icons/Save";
 import VideoCard from "./components/Card";
@@ -16,16 +17,21 @@ class App extends React.Component {
       error: false,
       errorText: "",
       isValid: false,
-      relatedVideos: []
+      relatedVideos: [],
+      showNotification: true
     }
   }
   getFormatFromStorage = () => {
     const format = localStorage.getItem("format") || "mp4";
     this.setState({ format });
   }
-  setFormatFromStorage = (format) => {
-    localStorage.setItem("format", format);
+  setFormatFromStorage = (format) => localStorage.setItem("format", format);
+  getNotificationFromStorage = () => {
+    const showNotification = localStorage.getItem("showNotification") || true;
+    const show = showNotification === "true" ? true : false;
+    this.setState({ showNotification: show });
   }
+  setNotificationFromStorage = (show) => localStorage.setItem("showNotification", `${show}`);
   isYouTubeUrl = (url) => {
     const ytRegex = new RegExp(/^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(\?\S*)?$/);
     return ytRegex.test(url);
@@ -62,6 +68,7 @@ class App extends React.Component {
     const host = window.location.hostname === "localhost" ? "localhost:4000" : window.location.hostname;
     window.open(`http://${host}/watch?v=${url}&format=${format}`);
     this.resetUrl();
+    this.setNotificationFromStorage(false);
     // const res = await API.get(`/download?watch=https://www.youtube.com/watch?v=_hbnMgHgZfs&format=mp4`);
     // const res = await API.post("/download", { url: "https://www.youtube.com/watch?v=_hbnMgHgZfs" });
   }
@@ -79,10 +86,11 @@ class App extends React.Component {
   componentDidMount = () => {
     this.addEnterEvent();
     this.getFormatFromStorage();
+    this.getNotificationFromStorage();
   }
   componentWillUnmount = () => this.removeEnterEvent();
   render() {
-    const { format, url, showThumbnail, title, thumbnailSrc, isValid, relatedVideos } = this.state;
+    const { format, url, showThumbnail, title, thumbnailSrc, isValid, relatedVideos, showNotification } = this.state;
     return (
       <Container>
         <Typography gutterBottom variant="h4">
@@ -99,41 +107,47 @@ class App extends React.Component {
           <RadioGroup aria-label="position" name="format" value={format} onChange={this.handleFormatChange} row>
             <FormControlLabel
               value="mp4"
-              control={<Radio color="primary" />}
+              control={<Radio color="primary" id="mp4" />}
               label="mp4"
               labelPlacement="end"
+              htmlFor="mp4"
             />
             <FormControlLabel
               value="mp3"
-              control={<Radio color="primary" />}
+              control={<Radio color="primary" id="mp3" />}
               label="mp3"
               labelPlacement="end"
+              htmlFor="mp3"
             />
             <FormControlLabel
               value="mov"
-              control={<Radio color="primary" />}
+              control={<Radio color="primary" id="mov" />}
               label="mov"
               labelPlacement="end"
+              htmlFor="mov"
             />
             <FormControlLabel
               value="flv"
-              control={<Radio color="primary" />}
+              control={<Radio color="primary" id="flv" />}
               label="flv"
               labelPlacement="end"
+              htmlFor="flv"
             />
           </RadioGroup>
         </div>
         <Button variant="contained" color="primary" fullWidth onClick={this.downloadVideo} startIcon={<SaveIcon />} disabled={!isValid}>Download</Button>
+        {showNotification && <Alert severity="info" style={{ marginTop: 35, fontWeight: "100" }}><p>Change <strong>www.youtube.com/watch?v=tERRFWuYG48</strong> to www.youtub<strong>dl</strong>e.com/watch?v=tERRFWuYG48 to directly download the video!</p></Alert>}
         {showThumbnail && (
           <div>
             <h5>{title}</h5>
             <img src={thumbnailSrc} alt={title} />
           </div>
         )}
-        <Typography gutterBottom variant="h4" style={{ marginTop: 100 }}>
-          Related videos
-        </Typography>
-        {!!relatedVideos.length && relatedVideos.map(video => (
+        {!!relatedVideos.length &&
+          <Typography gutterBottom variant="h4" style={{ marginTop: 100 }}>
+            Related videos
+        </Typography>}
+        {relatedVideos.map(video => (
           <React.Fragment key={video.id}>
             <VideoCard {...video} />
           </React.Fragment>
--- index.js
+++ index.js
@@ -3,67 +3,52 @@ const cors = require("cors");
 const ytdl = require("ytdl-core");
 const app = express();
 const port = process.env.PORT || 4000;
-app.use(express.static(__dirname + "/public"));
+
+app.use(express.static(__dirname));
 app.use(cors());
 app.use(express.json());
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
-// serve the homepage
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-// get the meta info from youtube
 app.post("/metainfo", async (req, res) => {
-    const videoURL = req.body.videoURL;
-    if (!videoURL) throw new Error("No video url!");
+    const { url } = req.body;
     try {
-        let result = await promiseInfo(videoURL);
-        res.send({ fail: false, ...result });
-    } catch (err) {
-        res.send(err.message).status(500);
-        throw err;
+        const result = await ytdl.getBasicInfo(url);
+        res.send({ ...result });
+    } catch (error) {
+        res.send(error.message).status(500);
+        throw error;
     }
-});
-// download the youtube video
-app.get("/download", async (req, res) => {
-    let videoURL = req.query.URL;
-    let videoFormat = req.query.FORMAT || "mp4";
-    let info;
-    try {
-        info = await promiseInfo(videoURL)
-            .catch((err) => {
-                throw err;
-            });
-    } catch (err) {
-        res.send(err.message).status(500);
-        throw err;
-    }
-    const title = info.title.replace(/[^a-zA-Z0-9-äöü ]/g, " ");
-    res.header('Content-disposition', 'attachment; filename=' + title + "." + videoFormat);
-    ytdl(videoURL, {
-        format: videoFormat,
-    })
-        .on("progress", (chunkLength, downloaded, total) => {
-            const download = (downloaded / 1024 / 1024).toFixed(2);
-            const tot = (total / 1024 / 1024).toFixed(2);
-            console.log(`${download}MB of ${tot}MB\n`);
-        })
-        .pipe(res)
-        .on("finish", () => {
-            // TODO: Clear URL input field
-            console.log("FINISHED!");
-        });
 });
 
-const promiseInfo = (videoURL) => {
-    return new Promise((resolve, reject) => {
-        ytdl.getInfo(videoURL, (err, info) => {
-            if (err) {
-                reject("NO VALID URL!");
-            }
-            resolve(info);
-        });
-    }).catch((err) => {
-        console.log("Error 1");
-    });
-}
+app.get("/watch", async (req, res) => {
+    const { v: url, format: f = "mp4" } = req.query;
+    const formats = ["mp4", "mp3", "mov", "flv"];
+    let format = f;
+    if (formats.includes(f)) {
+        format = f;
+    } else {
+        format = "mp4";
+    }
+    try {
+        const result = await ytdl.getBasicInfo(url);
+        const title = result.title.replace(/[^a-zA-Z0-9-äöü ]/g, " ");
+        res.header('Content-disposition', 'attachment; filename=' + title + "." + format);
+        ytdl(url, { format })
+            .on("progress", (chunkLength, downloaded, total) => {
+                const download = (downloaded / 1024 / 1024).toFixed(2);
+                const tot = (total / 1024 / 1024).toFixed(2);
+                console.log(`${download}MB of ${tot}MB\n`);
+            })
+            .pipe(res)
+            .on("finish", () => {
+                console.log("FINISHED!");
+            });
+    } catch (err) {
+        res.redirect(`http://${req.headers.host}?error=1`)
+    }
+});
