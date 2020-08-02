690d87190445784af634d6ef0cc1460e4ceacd94
Benny <meier.benjamin@web.de>
2020-08-02 14:09:59 +0200

    Feature improvements
    * Add youtube-search-api
    * Remove material-ui
    * Add new api endpoints
    * Optimized speed

--- index.js
+++ index.js
@@ -1,94 +1,84 @@
 const express = require("express");
 const cors = require("cors");
 const ytdl = require("ytdl-core");
+const searchYoutube = require("youtube-api-v3-search");
 const contentDisposition = require("content-disposition");
 const app = express();
 const port = process.env.PORT || 4000;
-const io = require("socket.io")();
-let isSocketOpen = false;
+const YOUTUBE_KEY = require("./youtube_key");
 
-io.on("connection", () => {
-    console.log("opened socket");
-    isSocketOpen = true;
-});
-
-io.on("disconnect", () => {
-    console.log("closed socket");
-    isSocketOpen = false;
-});
-
-io.listen(3030, () => {
-    console.log("Listening ... 🚀");
-});
-
-app.use(express.static(__dirname));
+// app.use(express.static(__dirname));
 app.use(cors());
 app.use(express.json());
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+// app.get("/", (req, res) => {
+//     res.sendFile(__dirname + "/index.html");
+// });
+
+app.get("/suggestions", async (req, res) => {
+  const { search } = req.query;
+  const options = {
+    q: search,
+    part: "snippet",
+    type: "video",
+  };
+  try {
+    const data = await searchYoutube(YOUTUBE_KEY, options);
+    const { items } = data;
+    return res.status(200).json({ success: true, data: items });
+  } catch (error) {
+    return res.status(400).json({ success: false, error });
+  }
 });
 
-app.post("/metainfo", async (req, res) => {
-    const { url } = req.body;
-    try {
-        const result = await ytdl.getBasicInfo(url);
-        res.send({ ...result });
-    } catch (error) {
-        res.send(error.message).status(500);
-        throw error;
-    }
+app.get("/metainfo", async (req, res) => {
+  const { url } = req.query;
+  if (!ytdl.validateID(url) && !ytdl.validateURL(url)) {
+    return res
+      .status(400)
+      .json({ success: false, error: "No valid YouTube Id!" });
+  }
+  try {
+    const result = await ytdl.getBasicInfo(url);
+    return res.status(200).json({ success: true, data: result });
+  } catch (error) {
+    return res.status(400).json({ success: false, error });
+  }
 });
 
 app.get("/watch", async (req, res) => {
-    const { v: url, format: f = "mp4" } = req.query;
-    try {
-        if (!ytdl.validateURL(url)) {
-            return;
-        }
-    } catch (err) {
-        console.log("error ", err);
-        res.redirect(`http://${req.headers.host}?error=wrongId`);
-    }
-    const formats = ["mp4", "mp3", "mov", "flv"];
-    let format = f;
-    if (formats.includes(f)) {
-        format = f;
-    } else {
-        format = "mp4";
-    }
-    try {
-        const result = await ytdl.getBasicInfo(url);
-        const { title } = result;
-        res.setHeader('Content-disposition', contentDisposition(`${title}.${format}`));
-        let downloadProgress = 0;
-        ytdl(url, { format })
-            .on("progress", (chunkLength, downloaded, total) => {
-                const download = (downloaded / 1024 / 1024).toFixed(2);
-                const tot = (total / 1024 / 1024).toFixed(2);
-                const progress = Math.ceil((download / tot) * 100);
-                // console.log(`${download}MB of ${tot}MB\n`);
-                if (isSocketOpen && progress !== downloadProgress) {
-                    downloadProgress = progress;
-                    console.log("progress ", progress);
-                    io.emit("download", JSON.stringify({ download: progress, total: tot }));
-                }
-            })
-            .pipe(res)
-            .on("close", () => {
-                if (isSocketOpen) {
-                    io.emit("downloadClose", JSON.stringify({ close: true }));
-                }
-            })
-            .on("finish", () => {
-                if (isSocketOpen) {
-                    io.emit("downloadEnd", JSON.stringify({ end: true }));
-                }
-            });
-    } catch (err) {
-        console.log("error ", err);
-        res.redirect(`http://${req.headers.host}?error=downloadError`);
-    }
+  const { v: url, format: f = "mp4" } = req.query;
+  if (!ytdl.validateID(url) && !ytdl.validateURL(url)) {
+    return res
+      .status(400)
+      .json({ success: false, error: "No valid YouTube Id!" });
+  }
+  const formats = ["mp4", "mp3", "mov", "flv"];
+  let format = f;
+  if (formats.includes(f)) {
+    format = f;
+  }
+  try {
+    const result = await ytdl.getBasicInfo(url);
+    const {
+      videoDetails: { title },
+    } = result;
+    res.setHeader(
+      "Content-disposition",
+      contentDisposition(`${title}.${format}`)
+    );
+    ytdl(url, { format })
+      .on("progress", (chunkLength, downloaded, total) => {
+        // const download = (downloaded / 1024 / 1024).toFixed(2);
+        // const tot = (total / 1024 / 1024).toFixed(2);
+        // const progress = Math.ceil((download / tot) * 100);
+        // console.log(`${download}MB of ${tot}MB\n`);
+      })
+      .pipe(res);
+  } catch (err) {
+    console.log("error ", err);
+    res.redirect(`http://${req.headers.host}?error=downloadError`);
+  }
 });
---
+++ youtube_key.js
@@ -0,0 +1,3 @@
+const YOUTUBE_KEY = "AIzaSyDitouVSFUB9-9nV5qLiUZjJKSx4SVNsOc";
+
+module.exports = YOUTUBE_KEY;
